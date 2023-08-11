e93a3e3c5c3b1b184c8cf1c311c17ab99d396dae
RamsesCamacho1171 <victorcamacho1171@gmail.com>
2023-08-11 12:46:11 -0600

    conexion a postgresql

--- back_building/settings.py
+++ back_building/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'back_building.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "db_edificio",
+        "USER": "usr_edificio",
+        "PASSWORD": "sasa",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
